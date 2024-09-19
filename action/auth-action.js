a469a9e811a70cf76b5ee8fc87cd9108f12c5a6f
shaival25 <shaivaldoshi5@gmail.com>
2024-09-19 10:54:05 +0530

    removed unwanted files

--- action/auth-action.js
+++
@@ -1,8 +0,0 @@
-"use server";
-import { registerUser } from "@/config/user.config";
-import { revalidatePath } from "next/cache";
-
-export const addUser = async (data) => {
-  const response = await registerUser(data);
-  return response;
-};
