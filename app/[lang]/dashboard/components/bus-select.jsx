b2e5366b0b179a5a5ddb2b2e825e8734ef78d7e9
sanjana-soc01 <sanjana.soc01@gmail.com>
2024-09-19 18:33:18 +0530

    Dashboard export functionality added

--- app/[lang]/dashboard/components/bus-select.jsx
+++ app/[lang]/dashboard/components/bus-select.jsx
@@ -1,87 +1,59 @@
-import { Button } from '@/components/ui/button'
-import handleError from '@/validation/unauthorized'
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { Button } from "@/components/ui/button";
 
-const BusSelect = ({ setSelectedBuses, selectedBuses }) => {
-  const [buses, setBuses] = useState([])
-  const router = useRouter()
-
-  const fetchAllBuses = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/bus`,
-        {
-          headers: {
-            'x-auth-token': Cookies.get('authToken')
-          }
-        }
-      )
-      if (response.status === 200) {
-        setBuses(response.data)
-      }
-    } catch (error) {
-      handleError(error, router)
-    }
-  }
-  useEffect(() => {
-    fetchAllBuses()
-  }, [])
-
-  const handleBusClick = busId => {
-    if (busId === 'all') {
-      if (!selectedBuses.includes('all')) {
+const BusSelect = ({ setSelectedBuses, selectedBuses, buses }) => {
+ 
+  const handleBusClick = (busId) => {
+    if (busId === "all") {
+      if (!selectedBuses.includes("all")) {
         // If 'All Buses' is clicked, set 'all' as the only selected
-        setSelectedBuses(['all'])
+        setSelectedBuses(["all"]);
       }
     } else {
       // Toggle the bus selection
-      setSelectedBuses(prevSelectedBuses => {
+      setSelectedBuses((prevSelectedBuses) => {
         // If 'all' is selected, deselect it when a specific bus is selected
-        let filteredSelection = prevSelectedBuses.filter(b => b !== 'all')
+        let filteredSelection = prevSelectedBuses.filter((b) => b !== "all");
 
         // Check if the bus is already selected
         if (filteredSelection.includes(busId)) {
           // If it's already selected, remove it from the array
-          filteredSelection = filteredSelection.filter(b => b !== busId)
+          filteredSelection = filteredSelection.filter((b) => b !== busId);
           if (filteredSelection.length === 0) {
-            return ['all']
+            return ["all"];
           }
-          return filteredSelection
+          return filteredSelection;
         } else {
           // If it's not selected, add it to the array
-          return [...filteredSelection, busId]
+          return [...filteredSelection, busId];
         }
-      })
+      });
     }
-  }
+  };
   return (
-    <div className='grid grid-cols-5 gap-2'>
+    <div className="grid grid-cols-5 gap-2">
       <Button
-        variant={selectedBuses.includes('all') ? 'contained' : 'outline'}
-        color='info'
-        className='col-span-1 lg:col-span-1'
-        onClick={() => handleBusClick('all')}
+        variant={selectedBuses.includes("all") ? "contained" : "outline"}
+        color="info"
+        className="col-span-1 lg:col-span-1"
+        onClick={() => handleBusClick("all")}
       >
         All Buses
       </Button>
-      {buses.map(bus => (
+      {buses.map((bus) => (
         <Button
           key={bus.macAddress}
           variant={
-            selectedBuses.includes(bus.macAddress) ? 'contained' : 'outline'
+            selectedBuses.includes(bus.macAddress) ? "contained" : "outline"
           }
-          color='primary'
-          className='col-span-1 lg:col-span-1'
+          color="primary"
+          className="col-span-1 lg:col-span-1"
           onClick={() => handleBusClick(bus.macAddress)}
         >
           {bus.busName}
         </Button>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default BusSelect
+export default BusSelect;
