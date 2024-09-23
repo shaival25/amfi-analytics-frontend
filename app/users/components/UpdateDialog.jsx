a312482bb8b2f5b07f0a52c080926c3904a612d1
sanjana-soc01 <sanjana.soc01@gmail.com>
2024-09-23 18:02:34 +0530

    Eye icon CSS updated

--- app/users/components/UpdateDialog.jsx
+++ app/users/components/UpdateDialog.jsx
@@ -1,74 +1,74 @@
-'use client'
-import { Icon } from '@iconify/react'
-import { Button } from '@/components/ui/button'
+"use client";
+import { Icon } from "@iconify/react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogClose
-} from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { useState } from 'react'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { useRouter } from 'next/navigation'
-import axios from 'axios'
-import Cookies from 'js-cookie'
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
-import handleError from '@/validation/unauthorized'
+  SelectValue,
+} from "@/components/ui/select";
+import handleError from "@/validation/unauthorized";
 
 const ToggleDialog = ({ icon, id, fetchRoles, fetchUserDetails }) => {
-  const [passwordType, setPasswordType] = useState('password')
+  const [passwordType, setPasswordType] = useState("password");
   const [updateUser, setUpdateUser] = useState({
-    username: '',
-    email: '',
-    password: '',
-    role: ''
-  })
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  });
 
-  const [roles, setRoles] = useState([])
+  const [roles, setRoles] = useState([]);
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const fetchUserById = async id => {
+  const fetchUserById = async (id) => {
     try {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/users/${id}`,
         {
           headers: {
-            'x-auth-token': Cookies.get('authToken')
-          }
+            "x-auth-token": Cookies.get("authToken"),
+          },
         }
-      )
+      );
       if (response.status === 200) {
-        setUpdateUser(response.data)
-        setRoles(await fetchRoles())
+        setUpdateUser(response.data);
+        setRoles(await fetchRoles());
       }
     } catch (error) {
-      handleError(error, router)
+      handleError(error, router);
     }
-  }
+  };
 
   const togglePasswordType = () => {
-    if (passwordType === 'text') {
-      setPasswordType('password')
-    } else if (passwordType === 'password') {
-      setPasswordType('text')
+    if (passwordType === "text") {
+      setPasswordType("password");
+    } else if (passwordType === "password") {
+      setPasswordType("text");
     }
-  }
+  };
 
   const saveUpdateUser = async () => {
-    if (updateUser.password === '') {
-      delete updateUser.password
+    if (updateUser.password === "") {
+      delete updateUser.password;
     }
     try {
       const response = await axios.put(
@@ -76,72 +76,72 @@ const ToggleDialog = ({ icon, id, fetchRoles, fetchUserDetails }) => {
         updateUser,
         {
           headers: {
-            'x-auth-token': Cookies.get('authToken')
-          }
+            "x-auth-token": Cookies.get("authToken"),
+          },
         }
-      )
+      );
       if (response.status === 200) {
-        fetchUserDetails()
-        return true
+        fetchUserDetails();
+        return true;
       }
-      return false
+      return false;
     } catch (error) {
-      console.error(error)
-      return false
+      console.error(error);
+      return false;
     }
-  }
-  const [activeIndex, setActiveIndex] = useState(1)
+  };
+  const [activeIndex, setActiveIndex] = useState(1);
   const handleNextSlide = async () => {
-    const updateUser = await saveUpdateUser()
+    const updateUser = await saveUpdateUser();
     if (updateUser) {
-      setActiveIndex(2)
+      setActiveIndex(2);
     } else {
-      setActiveIndex(3)
+      setActiveIndex(3);
     }
-  }
+  };
 
   return (
     <Dialog>
       <DialogTrigger onClick={() => fetchUserById(id)} asChild>
         {icon}
       </DialogTrigger>
-      <DialogContent size='2xl' className='p-0'>
-        <DialogHeader className='p-6 pb-2'>
-          <DialogTitle className='text-base font-medium'>
-            {' '}
+      <DialogContent size="2xl" className="p-0">
+        <DialogHeader className="p-6 pb-2">
+          <DialogTitle className="text-base font-medium">
+            {" "}
             Update User
           </DialogTitle>
         </DialogHeader>
-        <div className='max-h-[300px]'>
-          <ScrollArea className='h-full px-6'>
+        <div className="max-h-[300px]">
+          <ScrollArea className="h-full px-6">
             {activeIndex === 1 && (
-              <div className='sm:grid  sm:grid-cols-2 sm:gap-5 space-y-4 sm:space-y-0'>
-                <div className='flex flex-col gap-2'>
+              <div className="sm:grid  sm:grid-cols-2 sm:gap-5 space-y-4 sm:space-y-0">
+                <div className="flex flex-col gap-2">
                   <Label>User Name</Label>
                   <Input
                     value={updateUser?.username}
-                    onChange={e =>
+                    onChange={(e) =>
                       setUpdateUser({ ...updateUser, username: e.target.value })
                     }
-                    type='text'
-                    placeholder='Enter user name'
+                    type="text"
+                    placeholder="Enter user name"
                   />
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className="flex flex-col gap-2">
                   <Label>Email</Label>
                   <Input
                     value={updateUser?.email}
-                    onChange={e =>
+                    onChange={(e) =>
                       setUpdateUser({ ...updateUser, email: e.target.value })
                     }
-                    type='text'
-                    placeholder='Enter email'
+                    type="text"
+                    placeholder="Enter email"
                   />
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className="flex flex-col gap-2">
                   <Label>Role</Label>
                   <Select
-                    onValueChange={value =>
+                    onValueChange={(value) =>
                       setUpdateUser({ ...updateUser, role: value })
                     }
                   >
@@ -149,7 +149,7 @@ const ToggleDialog = ({ icon, id, fetchRoles, fetchUserDetails }) => {
                       <SelectValue placeholder={updateUser?.role?.name} />
                     </SelectTrigger>
                     <SelectContent>
-                      {roles?.map(role => (
+                      {roles?.map((role) => (
                         <SelectItem
                           key={role._id}
                           value={role._id}
@@ -161,80 +161,99 @@ const ToggleDialog = ({ icon, id, fetchRoles, fetchUserDetails }) => {
                     </SelectContent>
                   </Select>
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className="flex flex-col gap-2">
                   <Label>Password</Label>
-                  <Input
-                    type={passwordType}
-                    value={updateUser?.password}
-                    onChange={e =>
-                      setUpdateUser({ ...updateUser, password: e.target.value })
-                    }
-                    placeholder='Enter password'
-                  />
                   <div
-                    role='button'
-                    className='absolute top-1/2 -translate-y-1/2 ltr:right-4 rtl:left-4 cursor-pointer'
-                    onClick={togglePasswordType}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      position: "relative",
+                    }}
                   >
-                    {passwordType === 'password' ? (
-                      <Icon
-                        icon='heroicons:eye'
-                        className='w-5 h-5 text-default-400'
-                      />
-                    ) : (
-                      <Icon
-                        icon='heroicons:eye-slash'
-                        className='w-5 h-5 text-default-400'
-                      />
-                    )}
+                    <Input
+                      type={passwordType}
+                      value={updateUser?.password}
+                      onChange={(e) =>
+                        setUpdateUser({
+                          ...updateUser,
+                          password: e.target.value,
+                        })
+                      }
+                      placeholder="Enter password"
+                      style={{
+                        width: "100%",
+                      }}
+                    />
+                    <div
+                      role="button"
+                      onClick={togglePasswordType}
+                      style={{
+                        position: "absolute",
+                        top: "25%",
+                        right: "1%",
+                      }}
+                    >
+                      {passwordType === "password" ? (
+                        <Icon
+                          icon="heroicons:eye"
+                          className="w-5 h-5 text-default-400"
+                        />
+                      ) : (
+                        <Icon
+                          icon="heroicons:eye-slash"
+                          className="w-5 h-5 text-default-400"
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
             )}
 
             {activeIndex === 2 && (
-              <div className='flex flex-col items-center '>
-                <span className='text-7xl text-success'>
-                  <Icon icon='material-symbols:check-circle-outline' />
+              <div className="flex flex-col items-center ">
+                <span className="text-7xl text-success">
+                  <Icon icon="material-symbols:check-circle-outline" />
                 </span>
-                <h3 className='mt-3 text-success  text-2xl font-semibold'>
+                <h3 className="mt-3 text-success  text-2xl font-semibold">
                   Congratulations
                 </h3>
-                <p className='mt-4 text-lg font-semibold text-default-600'>
+                <p className="mt-4 text-lg font-semibold text-default-600">
                   User Updated
                 </p>
               </div>
             )}
             {activeIndex === 3 && (
-              <div className='flex flex-col items-center '>
-                <span className='text-7xl text-success'>
-                  <Icon icon='material-symbols:check-circle-outline' />
+              <div className="flex flex-col items-center ">
+                <span className="text-7xl text-success">
+                  <Icon icon="material-symbols:check-circle-outline" />
                 </span>
-                <h3 className='mt-3 text-success  text-2xl font-semibold'>
+                <h3 className="mt-3 text-success  text-2xl font-semibold">
                   Error
                 </h3>
-                <p className='mt-4 text-lg font-semibold text-default-600'>
+                <p className="mt-4 text-lg font-semibold text-default-600">
                   Cannot Update User
                 </p>
               </div>
             )}
           </ScrollArea>
         </div>
-        <div className='p-6 pt-4 flex justify-between'>
+        <div className="p-6 pt-4 flex justify-between">
           <DialogClose onClick={() => setActiveIndex(1)} asChild>
-            <Button type='button' color='warning'>
+            <Button type="button" color="warning">
               Close
             </Button>
           </DialogClose>
           {activeIndex === 1 && (
-            <Button type='submit' onClick={handleNextSlide}>
+            <Button type="submit" onClick={handleNextSlide}>
               Submit
             </Button>
           )}
         </div>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default ToggleDialog
+export default ToggleDialog;
