aabe2c85853cae5f06e45f4572355efa77e7efd3
shaival25 <shaivaldoshi5@gmail.com>
2024-10-02 16:00:41 +0530

    removed bus name from heatmap

--- app/dashboard/components/heat-maps/heat-map.jsx
+++ app/dashboard/components/heat-maps/heat-map.jsx
@@ -12,7 +12,7 @@ const HeatMap = ({ heatMaps }) => {
     <div className="grid grid-cols-4 gap-2 mt-4">
       {heatMaps?.map((heatMap, index) => (
         <div key={index} className="grid gap-2">
-          <Label>{heatMap.busName}</Label>
+          {/* <Label>{heatMap.busName}</Label> */}
           <img width={300} height={300} src={heatMap.image} alt="heatMap" />
           <div className="fs-6 text-gray-600">
             <span>Date: {heatMap.date}</span>
