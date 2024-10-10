8e583705124bb571a3bdea0fd9bdcd9566e3ad1a
shaival25 <shaivaldoshi5@gmail.com>
2024-10-10 18:32:32 +0530

    changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jy3JoVAElN8rl0cJ6YdDUECFl1JlOPlzJThFnuwDV2c=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"TrnEOhd0taCZefVXUZjbeAuJPV2FBBKErc9+IQbPLtM=\"}"
